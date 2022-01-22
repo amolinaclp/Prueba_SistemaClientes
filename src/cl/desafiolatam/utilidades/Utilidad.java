4e09d586d072706ebc10beca060c6073e0d4bf47
amolinachile <contacto2@andresmolina.org>
2022-01-22 00:19:24 -0300

    14.- Clase Utilidad
    Se crea la clase utilidad, se elimina en leeme.txt

---
+++ src/cl/desafiolatam/utilidades/Utilidad.java
@@ -0,0 +1,17 @@
+package cl.desafiolatam.utilidades;
+
+public class Utilidad {
+
+	public void limpiarPantalla() {
+		for(int i=0;i<20;i++) {
+			System.out.println("");
+		}
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void printLine(String mensaje) {
+		System.out.println(mensaje);
+	}
+
+}
