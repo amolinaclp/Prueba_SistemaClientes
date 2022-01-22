63d61144f3826c0b35e237390018042f582dfe8a
amolinachile <contacto2@andresmolina.org>
2022-01-21 23:39:35 -0300

    5.- Crear clase Main
    Clase instanciando Menu, en esta etapa la clase menú esta creada en s package vista, pero sin mayores detalles, solo para que no diera error al momento de instanciarla

---
+++ src/cl/desafiolatam/modelo/Main.java
@@ -0,0 +1,19 @@
+package cl.desafiolatam.main;
+
+import cl.desafiolatam.vista.Menu;
+import cl.desafiolatam.vista.MenuTemplate;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		MenuTemplate menu=new Menu();
+    	menu.iniciarMenu();
+    	 
+
+	}
+
+}
